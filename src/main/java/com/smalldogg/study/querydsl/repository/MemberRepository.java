e6f4a349739af1760f5f8c6c35ce2e70d2df78d5
small-dogg <sy920112@naver.com>
2022-05-24 09:49:59 +0900

    feat: 스프링 데이터 JPA가 제공하는 Querydsl 기능   - QuerydslPredicateExecutor   - Querydsl 지원 클래스 직접 만들기

--- src/main/java/com/smalldogg/study/querydsl/repository/MemberRepository.java
+++ src/main/java/com/smalldogg/study/querydsl/repository/MemberRepository.java
@@ -2,11 +2,12 @@ package com.smalldogg.study.querydsl.repository;
 
 import com.smalldogg.study.querydsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     //select m from Member m where m.username=?
     List<Member> findByUsername(String username);
 }
